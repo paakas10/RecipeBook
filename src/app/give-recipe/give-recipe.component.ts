26b4e5e2caaa5220986d8a6ffad63b80a6499f3d
aakash1730 <aakash3017@gmail.com>
2021-03-02 12:11:07 -0700

    Add files via upload

---
+++ src/app/give-recipe/give-recipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-give-recipe',
+  templateUrl: './give-recipe.component.html',
+  styleUrls: ['./give-recipe.component.css']
+})
+export class GiveRecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
